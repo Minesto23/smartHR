10a73c1d7c5abf194a8ac0aa5a5560f9960831c3
Miguel Morales <minesto23@gmail.com>
2019-10-24 23:39:51 -0400

    a

--- smarthr/settings.py
+++ smarthr/settings.py
@@ -128,7 +128,7 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
-# Internationalization
+# Internationalization-------------------------------
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
 # LANGUAGE_CODE = 'en-us'
